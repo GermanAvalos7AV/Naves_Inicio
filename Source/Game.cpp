7bb90c78dd65924542e2aa6ad33c054012b84b88
GermanAvalos7AV <ger_manavalos10@hotmail.com>
2014-09-22 21:35:26 -0800

    Tarea 2 corregida
    Igualando a cero que tenia el error ya esta corregido

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,13 +1,19 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "Game.h"
 
-CGame::CGame(){
-	estado == 0;//ACT2: Esto esta mal, estas haciendo una comparacion, lo que debes de hacer es inicializar tu estado.
+CGame::CGame()
+{
+	estado = ESTADO_INICIANDO;
+	//Corregido Maestro//
+
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
-void CGame::Finalize(){
+void CGame::Finalize()
+{
+
 }
 
 bool CGame::Start()
@@ -15,17 +21,25 @@ bool CGame::Start()
 	// Esta variable nos ayudara a controlar la salida del juego...
 	int salirJuego = false;
           
-	while (salirJuego == false){
+	while (salirJuego == false)
+	{
             
 		//Maquina de estados
-		switch(estado){////ACT 2: No suficiente, te falto agregar 2 estados mas de los 5 que les inique.
-			case Estado::ESTADO_INICIANDO:
+		switch(estado)
+		{
+			case Estado::ESTADO_INICIANDO: 
 				break;
-				case Estado::ESTADO_JUGANDO:
-			break;
-				case Estado::ESTADO_TERMINANDO:
-					salirJuego = true;
+			case Estado::ESTADO_MENU:
+				break;
+			case Estado::ESTADO_JUGANDO:
+				break;
+			case Estado::ESTADO_FINALIZANDO:
+				break;
+			case Estado::ESTADO_TERMINANDO: 
+				salirJuego = true;
 			break;
 		};
+    }
 	return true;
 }
+
